82f9244bab7850d5a1f1e81196e049124898abe6
santaev <santaevp@gmail.com>
2019-01-24 00:00:29 +0700

    add test for not enough train cars for placement

--- domain/src/test/kotlin/org/tumba/entity/CommandProcessorsTest.kt
+++ domain/src/test/kotlin/org/tumba/entity/CommandProcessorsTest.kt
@@ -11,7 +11,7 @@ class PlaceTrainCardsCommandProcessorTest {
 
     @Test(expected = IllegalTrainCarTypeException::class)
     fun testUnsuccessfulTrainCarPlacementCausedIncorrectCards() {
-        val gameState = provideGameState()
+        val gameState = provideGameState1()
         val commandProcessor = PlaceTrainCarsCommandProcessor(TrainCarPlacementValidator())
         // Road(0, cityMoscow, citySpb, 5, Road.Color.RED)
         val command = PlaceTrainCarsCommand(
@@ -25,7 +25,7 @@ class PlaceTrainCardsCommandProcessorTest {
         )
     }
 
-    private fun provideGameState(): GameState {
+    private fun provideGameState1(): GameState {
         val players = listOf(
             Player(0, "Player1", Player.Color.BLACK),
             Player(1, "Player2", Player.Color.BLUE)
@@ -53,6 +53,50 @@ class PlaceTrainCardsCommandProcessorTest {
                 )
             ),
             intermediateGameState = IntermediateGameState.PlacingTrainCars(players[0]),
+            cardsHolder = mockk()
+        )
+    }
+
+    @Test(expected = NotEnoughTrainCarsException::class)
+    fun testUnsuccessfulTrainCarPlacementCausedNotEnoughTrainCars() {
+        val gameState = provideGameState2()
+        val commandProcessor = PlaceTrainCarsCommandProcessor(TrainCarPlacementValidator())
+        // Road(0, cityMoscow, citySpb, 5, Road.Color.RED)
+        // Placing to road 0 with 5 length and RED color.
+        // Should be not enough train card
+        val command = PlaceTrainCarsCommand(
+            playerId = 1,
+            roadId = 0,
+            wagonCardIds = listOf(0, 1, 2, 3, 4)
+        )
+        commandProcessor.process(
+            gameData = ICommandProcessor.GameData(gameState, GameHelper(gameState)),
+            command = command
+        )
+    }
+
+    private fun provideGameState2(): GameState {
+        val players = listOf(
+            Player(0, "Player1", Player.Color.BLACK),
+            Player(1, "Player2", Player.Color.BLUE)
+        )
+        val trainCarCardsOfPlayer1 = (0..4).map { TrainCarCard(it, TrainCarCard.Kind.RED) }.toMutableList()
+        return GameState(
+            players = players,
+            map = TestThreeCityMap.map,
+            trainCarPlacements = mutableListOf(),
+            playerStates = PlayerStates(
+                states = listOf(
+                    mockk(),
+                    PlayerState( // Player2, id = 1
+                        numberOfTrainCars = 3,
+                        points = 0,
+                        destinationTicketCards = mutableListOf(),
+                        trainCarCards = trainCarCardsOfPlayer1
+                    )
+                )
+            ),
+            intermediateGameState = IntermediateGameState.PlacingTrainCars(players[1]),
             cardsHolder = mockk()
         )
     }
